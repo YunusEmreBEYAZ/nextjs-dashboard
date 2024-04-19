4e36536f4062654b36eeacb96bc321001c683cac
YunusEmreBEYAZ <yebeyaz@gmail.com>
2024-04-19 23:54:58 +0200

    seeding data

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-  UserIcon
+
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -20,7 +20,6 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-  { name: 'Settings', href: '/dashboard/settings', icon: UserIcon },
 ];
 
 export default function NavLinks() {
